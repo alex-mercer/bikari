ddc8bcdc8b3372629392475ef8d3591becf81d8a
Mohammad Amin Khashkhashi Moghaddam <amin.moghaddamV@gmail.com>
2015-05-15 12:15:18 +0430

    Create views

--- bikari/urls.py
+++ bikari/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from hacka import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'bikari.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.home),
+    url(r'^(?P<city>.+)/', views.show_city),
+    url(r'^/(?P<city>.+)/(?P<event_id>.+)/', views.show_event),
+    url(r'^/user/(?P<user_name>.+)/', views.show_user),
+
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- hacka/views.py
+++ hacka/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request,'index.html')
+
+
+def show_city(request, city):
+    pass
+
+
+def show_event(request, city, event_id):
+    pass
+
+
+def show_user(request, user_name):
+    pass
