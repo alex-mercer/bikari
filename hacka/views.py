4bf6142d5f24dec9f1e7cd584e133f3fa886f7cd
S.HASHEMI <hashemi.soroush@gmail.com>
2015-05-15 12:16:53 +0430

    Register view done(need tests)

--- hacka/views.py
+++ hacka/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import *
+
+def register_user(request):
+    if request.user.is_authenticated():
+        return redirect('/')
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    first_name = request.POST.get('first_name')
+    last_name = request.POST.get('last_name')
+    email = request.POST.get('email')
+
+    new_user = HackaUser()
+    new_user.user = User.objects.create_user(username=username, password=password, email=email)
+    new_user.user.first_name = first_name
+    new_user.user.last_name = last_name
+    new_user.save()
+
+    return redirect('/')
